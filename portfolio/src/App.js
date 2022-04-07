64eff5f7cecc2e7f8737e04ace009a0634f8d6f6
svrchi <kevinsarchi@yahoo.com>
2022-04-06 20:30:01 -0400

    about me component added

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,12 @@
 import "./App.css";
 import Intro from "./Components/intro/Intro";
+import About from "./Components/about/about";
 
 function App() {
   return (
     <div className="App">
       <Intro />
-      
+      <About />
     </div>
   );
 }
