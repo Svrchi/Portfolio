2c590f88db0ed11889dc37a3e11efc0b975574c8
svrchi <kevinsarchi@yahoo.com>
2022-04-25 23:56:12 -0400

    product component layout

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import Intro from "./Components/intro/Intro";
 import About from "./Components/about/about";
+import ProductList from "./Components/productList/productList";
 
 function App() {
   return (
     <div className="App">
       <Intro />
       <About />
+      <ProductList/>
     </div>
   );
 }
